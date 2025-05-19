c47d9aff5e40df487168fce8feb82c946b265c19
Himesh <himeshkumaranant@gmail.com>
2025-05-19 15:10:36 +0530

    Complete part 1a exercises

--- part_1/introdemo/src/App.jsx
+++ part_1/introdemo/src/App.jsx
@@ -1,29 +1,47 @@
-var Hello = (props) => {
-  console.log(props)
+const Header = (props) => {
+  return (
+    <h1>{props.name}</h1>
+  )
+}
+
+const Part = (props) => {
+  return (
+    <p>{props.name} : {props.exercise}</p>
+  )
+}
+
+const Content = (props) => {
+  return (
+    <div>
+      <Part name={props.part1} exercise={props.excercise1} />
+      <Part name={props.part2} exercise={props.excercise2} />
+      <Part name={props.part3} exercise={props.excercise3} />
+    </div>
+  )
+}
+
+const Total = (props) => {
   return (
     <>
-      <p>Hello {props.name}</p>
+      <p>Number of exercises {props.excercise1 + props.excercise2 + props.excercise3}</p>
     </>
   )
 }
 
 const App = () => {
-  const a = 10
-  const b = 20
-  const now = new Date()
-
-  const name = "lucifer"
-  const age = 699999
-
-  console.log(now, "Hey folks")
+  const course = 'Half stack application development'
+  const part1 = 'Fundamentals of React'
+  const excercise1 = 10
+  const part2 = 'Using props to pass Data'
+  const excercise2 = 7
+  const part3 = 'State of a component'
+  const excercise3 = 14
 
   return (
     <>
-      <Hello name={name} age={age}/>
-      <p>it is {now.toString()}</p>
-      <p>
-        {a} plus {b} equals {a+b}
-      </p>
+      <Header name={course}/>
+      <Content part1={part1} part2={part2} part3={part3} excercise1={excercise1} excercise2={excercise2} excercise3={excercise3} />
+      <Total excercise1={excercise1} excercise2={excercise2} excercise3={excercise3} />
     </>
   )
 }
---
+++ part_1/introdemo/src/test.js
@@ -0,0 +1,4 @@
+const x = [1, 2, 3, 4]
+
+const [a, b, c] = x
+console.log(a, b, c)
