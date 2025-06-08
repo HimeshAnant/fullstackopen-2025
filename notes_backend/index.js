91490c0e4adbdf696137dd4b83505f583e768f76
Himesh <himeshkumaranant@gmail.com>
2025-06-08 15:56:16 +0530

    added database part 3c

--- notes_backend/index.js
+++ notes_backend/index.js
@@ -1,12 +1,11 @@
+require('dotenv').config()
 const express = require('express')
-const cors = require('cors')
+const Note = require('./models/note')
 
 const app = express()
 
 app.use(express.static('dist'))
-
 app.use(express.json())
-app.use(cors())
 
 let notes = [
     {
@@ -31,30 +30,27 @@ app.get('/', (request, response) => {
 })
 
 app.get('/api/notes', (request, response) => {
-    response.json(notes)
+    Note.find({}).then(notes => {
+        response.json(notes)
+    })
 })
 
-app.get('/api/notes/:id', (request, response) => {
-    const id = request.params.id
-    const note = notes.find( note => note.id === id )
-    if (note) response.json(note)
-    else response.status(404).end()
+app.get('/api/notes/:id', (request, response, next) => {
+    Note.findById(request.params.id)
+        .then(note => {
+            if (note) response.json(note)
+            else response.status(404).end()
+        })
+        .catch(error => next(error))
 })
 
-app.delete('/api/notes/:id', (request, response) => {
-    const id = request.params.id
-    notes = notes.filter( note => note.id !== id )
-
-    response.status(204).end()
+app.delete('/api/notes/:id', (request, response, next) => {
+    Note.findByIdAndDelete(request.params.id)
+        .then(result => {
+            response.status(204).end()
+        })
+        .catch(error => next(error))
 })
-
-const generateId = () => {
-    const maxId = notes.length > 0
-        ? Math.max(...notes.map( note => note.id ))
-        : 0
-    
-    return maxId+1
-}
 
 app.post('/api/notes', (request, response) => {
     const body = request.body
@@ -65,17 +61,49 @@ app.post('/api/notes', (request, response) => {
         })
     }
 
-    const note = {
+    const note = new Note({
         content: body.content,
         important: body.important || false,
-        id: generateId(),
-    }
+    })
 
-    notes = notes.concat(note)
-    response.json(note)
+    note.save().then( savedNote => {
+        response.json(savedNote)
+    } )
+})
+
+app.put('/api/notes/:id', (request, response, next) => {
+    const { content, important } = request.body
+
+    Note.findById(request.params.id)
+        .then(note => {
+            if (!note) {
+                return response.status(404).end()
+            }
+
+            note.content = content
+            note.important = important
+
+            return note.save().then(updatedNote => {
+                response.json(updatedNote)
+            })
+        })
+        .catch(error => next(error))
 })
 
 const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`)
 })
+
+
+const errorHandler = (error, request, response, next) => {
+    console.log(error.message)
+
+    if (error.name === "CastError") {
+        return response.status(400).json({ error: 'malformated id'} )
+    }
+
+    next(error)
+}
+
+app.use(errorHandler)
---
+++ notes_backend/models/note.js
@@ -0,0 +1,29 @@
+const mongoose = require('mongoose')
+
+mongoose.set('strictQuery', false)
+
+uri = process.env.MONGODB_URI
+
+console.log("connecting to", uri)
+mongoose.connect(uri)
+    .then( result => {
+        console.log("connected to MongoDB")
+    } )
+    .catch( error => {
+        console.log('error connecting to MongoDB: ', error.message)
+    } )
+
+const noteSchema = mongoose.Schema({
+    content: String,
+    important: Boolean,
+})
+
+noteSchema.set('toJSON', {
+    transform: (document, returnedObject) => {
+        returnedObject.id = returnedObject._id
+        delete returnedObject._id
+        delete returnedObject.__v
+    }
+})
+
+module.exports = mongoose.model('Note', noteSchema)
